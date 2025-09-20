2830276b9abda06c0b985448361c4046af588e80
JErBerlin <jeronir@hotmail.com>
2025-09-20 19:28:02 +0200

    main launches go routines to deposit concurrently

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/jerberlin/examples-ch9bank2/bank"
+)
+
+func main() {
+	balance := bank.Balance()
+	println("Initial balance: ", balance)
+
+	for i := 1; i <= 100; i++ {
+		go func() {
+			// add some fixed amount
+			amount := 1
+			bank.Deposit(amount)
+		}()
+	}
+
+	// check final balance: should be 100
+	balance = bank.Balance()
+	println("Final balance: ", balance)
+}
